852312e90bbe5a511de339ab3b813b487c2ec6f3
Max <mloffgren@icloud.com>
2025-01-31 14:57:18 -0600

    Added Class

--- main.py
+++ main.py
@@ -1,76 +1,36 @@
-import aiohttp
-import asyncio
+from discord_bot import BlinkMonitor
+from monitor import StockMonitor
 from track_items import asins
 from dotenv import load_dotenv
 import os
-import discord
+import asyncio
+import aiohttp
 
-# load_dotenv(dotenv_path='/etc/secrets/API.env')
-load_dotenv(dotenv_path='/Users/maxloffgren/Documents/Amazon Monitor/API.env')
+class Application:
+    def __init__(self):
+        load_dotenv()
+        self.monitor = StockMonitor(
+            url=os.getenv('URL'),
+            headers={
+                'Content-Type': 'application/json',
+                'x-api-key': 'test-key'
+            }
+        )
+        self.discord_bot = BlinkMonitor(
+            token=os.getenv('DISCORD_TOKEN'),
+            channel_id=int(os.getenv('DISCORD_CHANNEL_ID'))
+        )
 
-# Private API endpoint
-url = str(os.getenv('URL'))
+    async def run_monitor(self):
+        async with aiohttp.ClientSession() as session:
+            while True:
+                tasks = [self.monitor.fetch_stock(session, asin) for asin in asins]
+                results = await asyncio.gather(*tasks)
+                for product in results:
+                    if product:
+                        await self.discord_bot.send_notification(product)
+                await asyncio.sleep(30)
 
-# Discord Requirements
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-headers = {
-    'Content-Type': 'application/json',
-    'x-api-key': 'test-key'
-}
-
-async def fetch_stock(session, asin):
-        data = {
-            'region': 'us',
-            'asin': asin
-        }
-        try: 
-            async with session.post(url, json=data, headers=headers) as response:
-                if response.status == 200:
-                    r_json = await response.json()
-                    # print(r_json)
-                    in_stock = r_json['product']['inStock']
-                    link = r_json['product']['link']
-                    offers = r_json.get('product', {}).get('offers', [])
-
-                    amazon_featured = any(offer.get('merchantName', '') == 'Amazon.com' for offer in offers)
-
-                    if in_stock and amazon_featured:
-                        print(f"Asin {asin} is in stock: {link}")
-                        return link
-                    else:
-                        print(f'Asin {asin} not in stock.')
-                elif response.status == 429:
-                     await asyncio.sleep(1)
-                     return await fetch_stock(session, asin)
-                else:
-                    print(f'Error occured for asin {asin}. Error Status: {response.status}')
-        
-        except Exception as e:
-             print(f"An error occured for ASIN {asin}: {e}")
-
-async def check_stock():
-    async with aiohttp.ClientSession() as session:
-        tasks = [fetch_stock(session, asin) for asin in asins]
-        results = await asyncio.gather(*tasks)
-        in_stock_links = [link for link in results if link is not None]
-
-        if in_stock_links:
-            print("In-stock product links:")
-            for link in in_stock_links:
-                print(link)
-                channel = client.get_channel(DISCORD_CHANNEL_ID)
-                await channel.send(f"🚨 **Product in stock!** 🚨\n{link}")
-
-
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
-    await check_stock()
-    await client.close()
-
-# Run the bot
-client.run(DISCORD_TOKEN)
+if __name__ == "__main__":
+    app = Application()
+    app.discord_bot.run()
